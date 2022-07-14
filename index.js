7b5ea8d7fc46470e2b099fcc5e87c8950a9e9164
JustEddie <skdpwls830@gmail.com>
2022-07-14 20:39:16 +1000

    commit tree 45979e3c98ec6c0079dddcc62bb8751b3db3d069

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+
+function wrapAdjective(param="*") {
+    return function encourageFunction(para="special") {
+        return `You are ${param}${para}${param}!`;
+    }
+}
+
+
+const Calculator = {
+    add : function(a,b) {
+        return a + b;
+    },
+    subtract : function(a,b) {
+        return a - b;
+    },
+    multiply : function(a,b) {
+        return a * b;
+    },
+    divide : function(a,b) {
+        return a / b;
+    }
+}
+
+console.log(Calculator.add(1,4));
+
+
+function actionApplyer(int, arr) {
+    let newInt = int
+    for(let i = 0; i < arr.length; i++) {
+        
+        newInt = arr[i](newInt);
+    }
+    return newInt;
+}
+
+const arrayOfTransforms = [
+    function(a){ return a * 2 },
+    function(a){ return a + 1000},
+    function(a){ return a % 7 }
+  ]
+
+actionApplyer(13,arrayOfTransforms);
